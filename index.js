ee79d1191c991a9977ecabb2e162cffbf91d44ca
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-18 14:32:42 +0700

    try another syntax query

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ connection.query("SELECT * FROM `todos`", function(err, results, fields) {
   console.log(results); // results contains rows returned by server
   console.log(fields); // fields contains extra meta data about results, if available
 });
+
+connection.execute(
+  'SELECT name, text FROM `users`,`todos` WHERE `name` = "Rina" AND `text` = "Learn SQL syntax"',
+  ["Rick C-137", 53],
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+
+    // If you execute same statement again, it will be picked form a LRU cache
+    // which will save query preparation time and give better performance
+  }
+);
